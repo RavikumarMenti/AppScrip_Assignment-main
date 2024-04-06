fa6d74e65565762901be2f746a3f5587b539b46b
ravikumar9095 <ravikumar.m.9095@gmail.com>
2024-04-06 20:08:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer/index";
+import Header from "./components/Header/index";
+import Home from "./components/Home/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
